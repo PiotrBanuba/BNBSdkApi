a789c8870436e8f1b26c05d7f9134282859d633d
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-16 12:18:19 +0300

    1.8.0-17

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-16"
+let bnbPackageVersion: Version = "1.8.0-17"
 
 let package = Package(
     name: "BNBSdkApi",
@@ -11,6 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "BNBSdkApi",
+            type: .dynamic,
             targets: [
                 "BNBSdkApi",
                 "BNBSdkApi_BNBSdkCore"
